ad3b7c7715d2f9d939131bbbb7199f36388e17e1
Gerlâne Lima <gerlima96@gmail.com>
2022-06-27 10:37:00 -0500

    Função addElement

--- project-02/public/scripts.js
+++ project-02/public/scripts.js
@@ -7,13 +7,22 @@ const form = document.querySelector('form');
 
 function addElement({ name, url }) {
     const list = [];
-    const item = {
+    const link = {
         nome: name,
         url: url
     }
-    console.log(item);
-    list.push(item);
-    console.log(list);
+    /*
+    Adiciona o link na lista que controla os elementos li na página index.html.
+    Dentro da função map, transforma cada item da lista em formato JSON, para que seja inserido
+    no elemento li de forma separada.
+    */
+    list.push(link);
+    list.map(function(item){
+        JSON.stringify(item);
+        let li = document.createElement('li');
+        li.appendChild(document.createElement('li')).textContent = `Nome: ${item.nome} - URL: ${item.url}`;
+        ul.appendChild(li);
+    });
 }
 
 function removeElement(element) {
@@ -25,8 +34,6 @@ form.addEventListener('submit', (event) => {
     O elemento preventDefault() pausa o eventlooping  para que ação do evento submit seja executada.
     */
     event.preventDefault();
-
-    addElement('Gerlâne', 'URL');
 
     let { value } = input;
 
@@ -46,8 +53,11 @@ form.addEventListener('submit', (event) => {
     */
     if (!url) 
         return alert('O texto não está formatado da maneira correta.');
-
-    if (!/^http/.test(url)) 
+    
+    // Antes !/^http/, não estava funcionando
+    //Expressão REGEX: testa se a string não começa com http
+        //^ No inicio buca a ?! negação, .* da combinação de qualquer caracter entre 0-ilimitadas vezes
+    if (/^(?!.*http)/.test(url)) 
         return alert('Digite a url da maneira correta.');
     /*
     Se os valores seguirem o formato estabelecido, o elemento é adicionado e o campo é esvaziado.
